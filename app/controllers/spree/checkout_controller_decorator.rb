902fad41fa3c5472ea0a9937b0c2760f0376822e
Nicolas Charlery <nicolas.charlery@gmail.com>
2016-03-17 15:45:57 -0400

    Refactor class_eval to prepend on CheckoutController

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,13 +1,19 @@
-Spree::CheckoutController.class_eval do
+module TaxCloud
+  module Spree
+    module CheckoutController
+      def self.prepended base
+        base.rescue_from SpreeTaxCloud::Error do |exception|
+          flash[:error] = exception.message
+          redirect_to checkout_state_path(:address)
+        end
 
-  rescue_from SpreeTaxCloud::Error do |exception|
-    flash[:error] = exception.message
-    redirect_to checkout_state_path(:address)
+        base.rescue_from TaxCloud::Errors::ApiError do |exception|
+          flash[:error] = Spree.t("address_verification_failed")
+          redirect_to checkout_state_path(:address)
+        end
+      end
+    end
   end
-
-  rescue_from TaxCloud::Errors::ApiError do |exception|
-    flash[:error] = Spree.t("address_verification_failed")
-    redirect_to checkout_state_path(:address)
-  end
-
 end
+
+Spree::CheckoutController.prepend ::TaxCloud::Spree::CheckoutController
