38c5a59a86a0e9fbb445c528f67078203ddb6e32
Nicolas Charlery <nicolas.charlery@gmail.com>
2016-03-17 15:47:04 -0400

    Remove Methods Associated With alias_chain_method

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,27 +1,11 @@
-Spree::OrderContents.class_eval do
-  def add_with_tax_cloud(variant, quantity = 1, options = {})
-    add_without_tax_cloud(variant, quantity, options).tap do
-      tax_cloud_compute_tax
+module TaxCloud
+  module Spree
+    module OrderContents
+      def tax_cloud_compute_tax
+        SpreeTaxCloud::TaxComputer.new(order).compute
+      end
     end
   end
-
-  def remove_with_tax_cloud(variant, quantity = 1, options = {})
-    remove_without_tax_cloud(variant, quantity).tap do
-      tax_cloud_compute_tax
-    end
-  end
-
-  def update_cart_with_tax_cloud(params)
-    if update_cart_without_tax_cloud(params)
-      tax_cloud_compute_tax
-      true
-    else
-      false
-    end
-  end
-
-  def tax_cloud_compute_tax
-    SpreeTaxCloud::TaxComputer.new(order).compute
-  end
-
 end
+
+Spree::OrderContents.prepend ::TaxCloud::Spree::OrderContents
