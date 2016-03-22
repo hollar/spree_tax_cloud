44bd7c4636e18cf08bd556c0c9cb635ec4edc369
Nicolas Charlery <nicolas.charlery@gmail.com>
2016-03-21 20:19:13 -0400

    Avoid Taxation When No Tax on Order
    If we try to tax an order that has no tax set, this will result in an
    error. Instead, we want to complete the order and deal with the tax in
    the customer service.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,41 +1,37 @@
 module Spree
+  NewOrder = true
 
-	NewOrder = true
-	
-	Order.class_eval do
+  Order.class_eval do
+    has_one :tax_cloud_transaction
 
-		has_one :tax_cloud_transaction
+    self.state_machine.before_transition :to => :delivery, :do => :lookup_tax_cloud, :if => :tax_cloud_eligible?
+    self.state_machine.before_transition :to => :payment, :do => :lookup_tax_cloud, :if => :tax_cloud_eligible?
+    self.state_machine.after_transition :to => :complete, :do => :capture_and_authorize_tax_cloud, :if => :tax_cloud_eligible?
 
-		self.state_machine.before_transition :to => :delivery, :do => :lookup_tax_cloud, :if => :tax_cloud_eligible?
+    def tax_cloud_eligible?
+      line_items.present? && ship_address.try(:state_id?)
+    end
 
-		self.state_machine.before_transition :to => :payment, :do => :lookup_tax_cloud, :if => :tax_cloud_eligible?
+    def lookup_tax_cloud
+      if tax_cloud_transaction.nil?
+        create_tax_cloud_transaction
+      end
+      tax_cloud_compute_tax
+    end
 
+    def capture_and_authorize_tax_cloud
+      return if additional_tax_total == 0
+      transaction = Spree::TaxCloudTransaction.transaction_with_taxcloud(self, NewOrder)
+      transaction.authorized_with_capture
+    end
 
-		self.state_machine.after_transition :to => :complete, :do => :capture_and_authorize_tax_cloud, :if => :tax_cloud_eligible?
+    def promotion_adjustment_total
+      adjustments.promotion.eligible.sum(:amount).abs
+    end
 
-		def tax_cloud_eligible?
-			line_items.present? && ship_address.try(:state_id?)
-		end
-
-		def lookup_tax_cloud
-			if tax_cloud_transaction.nil?
-				create_tax_cloud_transaction
-			end
-			tax_cloud_compute_tax
-		end
-
-		def capture_and_authorize_tax_cloud
-			transaction = Spree::TaxCloudTransaction.transaction_with_taxcloud(self, NewOrder)
-			transaction.authorized_with_capture 
-		end
-
-		def promotion_adjustment_total
-			adjustments.promotion.eligible.sum(:amount).abs
-		end
-
-		# Compute  taxcloud, but do not save 
-		def tax_cloud_compute_tax
-			SpreeTaxCloud::TaxComputer.new(self).compute
-		end
-	end
+    # Compute  taxcloud, but do not save
+    def tax_cloud_compute_tax
+      SpreeTaxCloud::TaxComputer.new(self).compute
+    end
+  end
 end
